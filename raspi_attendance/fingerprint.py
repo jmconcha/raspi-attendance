49e7a3b1bcbc1f49d4b3e2609fd514bc72c7a089
Jayson Concha <jmconcha.pro@gmail.com>
2025-02-23 18:38:10 +0800

    refactor

--- raspi_attendance/fingerprint.py
+++ raspi_attendance/fingerprint.py
@@ -8,48 +8,111 @@ import busio
 from digitalio import DigitalInOut, Direction
 import adafruit_fingerprint
 
+led = DigitalInOut(board.D13)
+led.direction = Direction.OUTPUT
 
-class FPrintScanner():
-    uart = serial.Serial("/dev/ttyS0", baudrate=57600, timeout=1)
-    finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
+# uart = busio.UART(board.TX, board.RX, baudrate=57600)
 
-    def __init__(self):
-        self.led = DigitalInOut(board.D13)
-        self.led.direction = Direction.OUTPUT
+# If using with a computer such as Linux/RaspberryPi, Mac, Windows with USB/serial converter:
+# import serial
+# uart = serial.Serial("/dev/ttyUSB0", baudrate=57600, timeout=1)
 
-    def get_fingerprint(self):
-        """Get a finger print image, template it, and see if it matches!"""
-        print("Waiting for image...")
-        while self.finger.get_image() != adafruit_fingerprint.OK:
-            pass
-        print("Templating...")
-        if self.finger.image_2_tz(1) != adafruit_fingerprint.OK:
-            return False
-        print("Searching...")
-        if self.finger.finger_search() != adafruit_fingerprint.OK:
-            return False
-        return True
+# If using with Linux/Raspberry Pi and hardware UART:
+uart = serial.Serial("/dev/ttyS0", baudrate=57600, timeout=1)
+print("test")
+print(uart)
 
-    # pylint: disable=too-many-branches
+finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
 
-    def get_fingerprint_detail(self):
-        """Get a finger print image, template it, and see if it matches!
-        This time, print out each error instead of just returning on failure"""
-        print("Getting image...", end="")
-        i = self.finger.get_image()
-        if i == adafruit_fingerprint.OK:
-            print("Image taken")
+##################################################
+
+
+def get_fingerprint():
+    """Get a finger print image, template it, and see if it matches!"""
+    print("Waiting for image...")
+    while finger.get_image() != adafruit_fingerprint.OK:
+        pass
+    print("Templating...")
+    if finger.image_2_tz(1) != adafruit_fingerprint.OK:
+        return False
+    print("Searching...")
+    if finger.finger_search() != adafruit_fingerprint.OK:
+        return False
+    return True
+
+
+# pylint: disable=too-many-branches
+def get_fingerprint_detail():
+    """Get a finger print image, template it, and see if it matches!
+    This time, print out each error instead of just returning on failure"""
+    print("Getting image...", end="")
+    i = finger.get_image()
+    if i == adafruit_fingerprint.OK:
+        print("Image taken")
+    else:
+        if i == adafruit_fingerprint.NOFINGER:
+            print("No finger detected")
+        elif i == adafruit_fingerprint.IMAGEFAIL:
+            print("Imaging error")
         else:
+            print("Other error")
+        return False
+
+    print("Templating...", end="")
+    i = finger.image_2_tz(1)
+    if i == adafruit_fingerprint.OK:
+        print("Templated")
+    else:
+        if i == adafruit_fingerprint.IMAGEMESS:
+            print("Image too messy")
+        elif i == adafruit_fingerprint.FEATUREFAIL:
+            print("Could not identify features")
+        elif i == adafruit_fingerprint.INVALIDIMAGE:
+            print("Image invalid")
+        else:
+            print("Other error")
+        return False
+
+    print("Searching...", end="")
+    i = finger.finger_fast_search()
+    # pylint: disable=no-else-return
+    # This block needs to be refactored when it can be tested.
+    if i == adafruit_fingerprint.OK:
+        print("Found fingerprint!")
+        return True
+    else:
+        if i == adafruit_fingerprint.NOTFOUND:
+            print("No match found")
+        else:
+            print("Other error")
+        return False
+
+
+# pylint: disable=too-many-statements
+def enroll_finger(location):
+    """Take a 2 finger images and template it, then store in 'location'"""
+    for fingerimg in range(1, 3):
+        if fingerimg == 1:
+            print("Place finger on sensor...", end="")
+        else:
+            print("Place same finger again...", end="")
+
+        while True:
+            i = finger.get_image()
+            if i == adafruit_fingerprint.OK:
+                print("Image taken")
+                break
             if i == adafruit_fingerprint.NOFINGER:
-                print("No finger detected")
+                print(".", end="")
             elif i == adafruit_fingerprint.IMAGEFAIL:
                 print("Imaging error")
+                return False
             else:
                 print("Other error")
-            return False
+                return False
 
         print("Templating...", end="")
-        i = self.finger.image_2_tz(1)
+        i = finger.image_2_tz(fingerimg)
         if i == adafruit_fingerprint.OK:
             print("Templated")
         else:
@@ -63,110 +126,59 @@ class FPrintScanner():
                 print("Other error")
             return False
 
-        print("Searching...", end="")
-        i = self.finger.finger_fast_search()
-        # pylint: disable=no-else-return
-        # This block needs to be refactored when it can be tested.
-        if i == adafruit_fingerprint.OK:
-            print("Found fingerprint!")
-            return True
+        if fingerimg == 1:
+            print("Remove finger")
+            time.sleep(1)
+            while i != adafruit_fingerprint.NOFINGER:
+                i = finger.get_image()
+
+    print("Creating model...", end="")
+    i = finger.create_model()
+    if i == adafruit_fingerprint.OK:
+        print("Created")
+    else:
+        if i == adafruit_fingerprint.ENROLLMISMATCH:
+            print("Prints did not match")
         else:
-            if i == adafruit_fingerprint.NOTFOUND:
-                print("No match found")
-            else:
-                print("Other error")
-            return False
+            print("Other error")
+        return False
 
-    # pylint: disable=too-many-statements
-
-    def enroll_finger(self, location):
-        """Take a 2 finger images and template it, then store in 'location'"""
-        for fingerimg in range(1, 3):
-            if fingerimg == 1:
-                print("Place finger on sensor...", end="")
-            else:
-                print("Place same finger again...", end="")
-
-            while True:
-                i = self.finger.get_image()
-                if i == adafruit_fingerprint.OK:
-                    print("Image taken")
-                    break
-                if i == adafruit_fingerprint.NOFINGER:
-                    print(".", end="")
-                elif i == adafruit_fingerprint.IMAGEFAIL:
-                    print("Imaging error")
-                    return False
-                else:
-                    print("Other error")
-                    return False
-
-            print("Templating...", end="")
-            i = self.finger.image_2_tz(fingerimg)
-            if i == adafruit_fingerprint.OK:
-                print("Templated")
-            else:
-                if i == adafruit_fingerprint.IMAGEMESS:
-                    print("Image too messy")
-                elif i == adafruit_fingerprint.FEATUREFAIL:
-                    print("Could not identify features")
-                elif i == adafruit_fingerprint.INVALIDIMAGE:
-                    print("Image invalid")
-                else:
-                    print("Other error")
-                return False
-
-            if fingerimg == 1:
-                print("Remove finger")
-                time.sleep(1)
-                while i != adafruit_fingerprint.NOFINGER:
-                    i = self.finger.get_image()
-
-        print("Creating model...", end="")
-        i = self.finger.create_model()
-        if i == adafruit_fingerprint.OK:
-            print("Created")
+    print("Storing model #%d..." % location, end="")
+    i = finger.store_model(location)
+    if i == adafruit_fingerprint.OK:
+        print("Stored")
+    else:
+        if i == adafruit_fingerprint.BADLOCATION:
+            print("Bad storage location")
+        elif i == adafruit_fingerprint.FLASHERR:
+            print("Flash storage error")
         else:
-            if i == adafruit_fingerprint.ENROLLMISMATCH:
-                print("Prints did not match")
-            else:
-                print("Other error")
-            return False
+            print("Other error")
+        return False
 
-        print("Storing model #%d..." % location, end="")
-        i = self.finger.store_model(location)
-        if i == adafruit_fingerprint.OK:
-            print("Stored")
-        else:
-            if i == adafruit_fingerprint.BADLOCATION:
-                print("Bad storage location")
-            elif i == adafruit_fingerprint.FLASHERR:
-                print("Flash storage error")
-            else:
-                print("Other error")
-            return False
+    return True
 
-        return True
+
+##################################################
+
+
+def get_num():
+    """Use input() to get a valid number from 1 to 127. Retry till success!"""
+    i = 0
+    while (i > 127) or (i < 1):
+        try:
+            i = int(input("Enter ID # from 1-127: "))
+        except ValueError:
+            pass
+    return i
 
 
 def main():
-    def get_num():
-        """Use input() to get a valid number from 1 to 127. Retry till success!"""
-        i = 0
-        while (i > 127) or (i < 1):
-            try:
-                i = int(input("Enter ID # from 1-127: "))
-            except ValueError:
-                pass
-        return i
-
-    fprint_scan = FPrintScanner()
-
     while True:
         print("----------------")
-        if fprint_scan.finger.read_templates() != adafruit_fingerprint.OK:
+        if finger.read_templates() != adafruit_fingerprint.OK:
             raise RuntimeError("Failed to read templates")
-        print("Fingerprint templates:", fprint_scan.finger.templates)
+        print("Fingerprint templates:", finger.templates)
         print("e) enroll print")
         print("f) find print")
         print("d) delete print")
@@ -174,15 +186,15 @@ def main():
         c = input("> ")
 
         if c == "e":
-            fprint_scan.enroll_finger(get_num())
+            enroll_finger(get_num())
         if c == "f":
-            if fprint_scan.get_fingerprint():
-                print("Detected #", fprint_scan.finger.finger_id,
-                      "with confidence", fprint_scan.finger.confidence)
+            if get_fingerprint():
+                print("Detected #", finger.finger_id,
+                      "with confidence", finger.confidence)
             else:
                 print("Finger not found")
         if c == "d":
-            if fprint_scan.finger.delete_model(get_num()) == adafruit_fingerprint.OK:
+            if finger.delete_model(get_num()) == adafruit_fingerprint.OK:
                 print("Deleted!")
             else:
                 print("Failed to delete")
--- raspi_attendance/mailer.py
+++ raspi_attendance/mailer.py
@@ -1,29 +1,19 @@
 from dotenv import load_dotenv
 import yagmail
 import os
-from pathlib import Path
 
 load_dotenv()
+CLIENT_EMAIL = os.getenv("EMAIL")
 
 
-class Mailer:
-    CLIENT_EMAIL = os.getenv("EMAIL")
-    PROJECT_ROOT_DIR = Path(__file__).resolve().parent.parent
-    OAUTH2_CREDS_FILEPATH = PROJECT_ROOT_DIR / "oauth2_creds.json"
+def send_email(receiver_email, receiver_name, file_attachment):
+    body = f"Hello {receiver_name}.\nAttached in this email is your attendance report."
 
-    def __init__(self, receiver_email, receiver_name, file_attachment):
-        self.receiver_email = receiver_email
-        self.receiver_name = receiver_name
-        self.file_attachment = file_attachment
-
-    def send_email(self):
-        body = f"Hello {self.receiver_name}.\nAttached in this email is your attendance report."
-
-        yag = yagmail.SMTP(self.CLIENT_EMAIL,
-                           oauth2_file=self.OAUTH2_CREDS_FILEPATH)
-        yag.send(
-            to=self.receiver_email,
-            subject="Attendance Report",
-            contents=body,
-            attachments=self.file_attachment,
-        )
+    yag = yagmail.SMTP(CLIENT_EMAIL,
+                       oauth2_file="./oauth2_creds.json")
+    yag.send(
+        to=receiver_email,
+        subject="Attendance Report",
+        contents=body,
+        attachments=file_attachment,
+    )
